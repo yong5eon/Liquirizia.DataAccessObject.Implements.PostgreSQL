71bb7dee8f904b0720f7c2d302ffa802539a546b
허용선 <yongseon.heo@gmail.com>
2025-03-22 00:05:12 +0900

    JSONB 타입에 인덱스를 생성할 수 있도록 지원

---
+++ src/Exprs/JavaScriptObjectNotation.py
@@ -0,0 +1,38 @@
+# -*- coding: utf-8 -*-
+
+from ..Expr import Expr
+from ..Column import Column
+from ..Type import Type
+
+from typing import Union, Type as T
+
+__all__ = (
+	'Of',
+)
+
+
+class Of(Expr):
+	def __init__(
+		self,
+		col: Union[str, Column, Expr],
+		arg: str,
+		type: Union[str, T[Type]] = None,
+	):
+		self.col = col
+		if isinstance(col, str):
+			self.col = Column(col)
+		self.arg = arg
+		self.type = type
+		return
+	def __str__(self):
+		return '({}->{}){}'.format(
+			str(self.col),
+			'\'{}\''.format(self.arg) if isinstance(self.arg, str) else str(self.arg),
+			'::{}'.format(self.type) if self.type else '',
+		)
+	def cast(
+		self,
+		type: Union[str, T[Type]],
+	):
+		self.type = type if isinstance(type, str) else str(type)
+		return self
