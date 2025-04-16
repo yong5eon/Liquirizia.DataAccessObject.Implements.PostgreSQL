536204b351d24ab65f7eee0b3e4748cb6784ba81
허용선 <yongseon.heo@gmail.com>
2025-04-16 16:44:33 +0900

    Timestamp, Date, Time 에서 해당 부분을 분리 할 수 있는 상수 및 함수 추가

--- sample/81.Timestamp.py
+++ sample/81.Timestamp.py
@@ -74,4 +74,81 @@ PrettyPrint(_)
 print(_.colTimestampTZ.astimezone(THA))
 print(_.colTimestampTZD.astimezone(THA))
 
+_ = con.run(Get(SampleTable).values(
+	Alias(SampleTable.colTimestamp, 'ts'),
+	Alias(Year(SampleTable.colTimestamp), 'year'),
+	Alias(Month(SampleTable.colTimestamp), 'month'),
+	Alias(Day(SampleTable.colTimestamp), 'day'),
+	Alias(Hour(SampleTable.colTimestamp), 'hour'),
+	Alias(Minute(SampleTable.colTimestamp), 'minute'),
+	Alias(Second(SampleTable.colTimestamp), 'second'),
+	Alias(MilliSecond(SampleTable.colTimestamp), 'millisecond'),
+	Alias(MicroSecond(SampleTable.colTimestamp), 'microsecond'),
+	Alias(Quarter(SampleTable.colTimestamp), 'quarter'),
+	Alias(Week(SampleTable.colTimestamp), 'week'),
+	Alias(DayOfWeek(SampleTable.colTimestamp), 'dayofweek'),
+	Alias(DayOfWeekISO(SampleTable.colTimestamp), 'dayofweekiso'),
+	Alias(DayOfYear(SampleTable.colTimestamp), 'dayofyear'),
+	Alias(Millenium(SampleTable.colTimestamp), 'millenium'),
+	Alias(Century(SampleTable.colTimestamp), 'century'),
+	Alias(Decade(SampleTable.colTimestamp), 'decade'),
+	Alias(EPoch(SampleTable.colTimestamp), 'epoch'),
+))
+PrettyPrint(_)
+
+_ = con.run(Get(SampleTable).values(
+	Alias(SampleTable.colTimestampTZ, 'ts'),
+	Alias(Year(SampleTable.colTimestampTZ), 'year'),
+	Alias(Month(SampleTable.colTimestampTZ), 'month'),
+	Alias(Day(SampleTable.colTimestampTZ), 'day'),
+	Alias(Hour(SampleTable.colTimestampTZ), 'hour'),
+	Alias(Minute(SampleTable.colTimestampTZ), 'minute'),
+	Alias(Second(SampleTable.colTimestampTZ), 'second'),
+	Alias(MilliSecond(SampleTable.colTimestampTZ), 'millisecond'),
+	Alias(MicroSecond(SampleTable.colTimestampTZ), 'microsecond'),
+	Alias(Quarter(SampleTable.colTimestampTZ), 'quarter'),
+	Alias(Week(SampleTable.colTimestampTZ), 'week'),
+	Alias(DayOfWeek(SampleTable.colTimestampTZ), 'dayofweek'),
+	Alias(DayOfWeekISO(SampleTable.colTimestampTZ), 'dayofweekiso'),
+	Alias(DayOfYear(SampleTable.colTimestampTZ), 'dayofyear'),
+	Alias(Timezone(SampleTable.colTimestampTZ), 'timezone'),
+	Alias(TimezoneHour(SampleTable.colTimestampTZ), 'timezonehour'),
+	Alias(TimezoneMinute(SampleTable.colTimestampTZ), 'timezoneminute'),
+	Alias(TimezoneHourMinute(SampleTable.colTimestampTZ), 'timezonehourminute'),
+	Alias(Millenium(SampleTable.colTimestampTZ), 'millenium'),
+	Alias(Century(SampleTable.colTimestampTZ), 'century'),
+	Alias(Decade(SampleTable.colTimestampTZ), 'decade'),
+	Alias(EPoch(SampleTable.colTimestampTZ), 'epoch'),
+))
+PrettyPrint(_)
+
+_ = con.run(Get(SampleTable).values(
+	Alias(SampleTable.colDate, 'dt'),
+	Alias(Year(SampleTable.colDate), 'year'),
+	Alias(Month(SampleTable.colDate), 'month'),
+	Alias(Day(SampleTable.colDate), 'day'),
+	Alias(Quarter(SampleTable.colDate), 'quarter'),
+	Alias(Week(SampleTable.colDate), 'week'),
+	Alias(DayOfWeek(SampleTable.colDate), 'dayofweek'),
+	Alias(DayOfWeekISO(SampleTable.colDate), 'dayofweekiso'),
+	Alias(DayOfYear(SampleTable.colDate), 'dayofyear'),
+	Alias(Millenium(SampleTable.colDate), 'millenium'),
+	Alias(Century(SampleTable.colDate), 'century'),
+	Alias(Decade(SampleTable.colDate), 'decade'),
+	Alias(EPoch(SampleTable.colDate), 'epoch'),
+))
+PrettyPrint(_)
+
+_ = con.run(Get(SampleTable).values(
+	Alias(SampleTable.colTime, 'ts'),
+	Alias(Hour(SampleTable.colTime), 'hour'),
+	Alias(Minute(SampleTable.colTime), 'minute'),
+	Alias(Second(SampleTable.colTime), 'second'),
+	Alias(MilliSecond(SampleTable.colTime), 'millisecond'),
+	Alias(MicroSecond(SampleTable.colTime), 'microsecond'),
+	Alias(EPoch(SampleTable.colTime), 'epoch'),
+))
+PrettyPrint(_)
+
+
 con.commit()
--- src/Functions/DateTime.py
+++ src/Functions/DateTime.py
@@ -1,9 +1,36 @@
 # -*- coding: utf-8 -*-
 
 from ..Function import Function
+from ..Column import Column
+from ..Expr import Expr
+from enum import Enum
+from typing import Union
 
 __all__ = (
-	'Now'
+	'Now',
+	'DatePartType',
+	'DatePart',
+	'Year',
+	'Month',
+	'Day',
+	'Hour',
+	'Minute',
+	'Second',
+	'MilliSecond',
+	'MicroSecond',
+	'Quarter',
+	'Week',
+	'DayOfWeek',
+	'DayOfWeekISO',
+	'DayOfYear',
+	'Timezone',
+	'TimezoneHour',
+	'TimezoneMinute',
+	'TimezoneHourMinute',
+	'Millenium',
+	'Century',
+	'Decade',
+	'EPoch',
 )
 
 
@@ -12,3 +39,163 @@ class Now(Function):
 		return
 	def __str__(self):
 		return 'NOW()'
+
+
+class DatePartType(str, Enum):
+	Year = 'YEAR'
+	Month = 'MONTH'
+	Day = 'DAY'
+	Hour = 'HOUR'
+	Minute = 'MINUTE'
+	Second = 'SECOND'
+	MilliSecond = 'MILLISECOND'
+	MicroSecond = 'MICROSECOND'
+	Quarter = 'QUARTER'
+	Week = 'WEEK'
+	DayOfWeek = 'DOW'
+	DayOfWeekISO = 'ISODOW'
+	DayOfYear = 'DOY'
+	Timezone = 'TIMEZONE'
+	TimezoneHour = 'TIMEZONE_HOUR'
+	TimezoneMinute = 'TIMEZONE_MINUTE'
+	TimezoneHourMinute = 'TIMEZONE_HOUR_MINUTE'
+	Millenium = 'MILLENNIUM'
+	Centry = 'CENTURY'
+	Decade = 'DECADE'
+	EPoch = 'EPOCH'
+	def __str__(self): return self.value
+
+
+class DatePart(Function):
+	def __init__(self, part: Union[DatePartType, str], expr: Union[Expr, Column, str]):
+		self.part = part
+		self.expr = expr
+		return
+	def __str__(self):
+		return 'DATE_PART(\'{}\', {})'.format(str(self.part), str(self.expr))
+
+
+class Year(DatePart):
+	def __init__(self, expr: Union[Expr, Column, str]):
+		super().__init__(DatePartType.Year, expr)
+		return
+
+
+class Month(DatePart):
+	def __init__(self, expr: Union[Expr, Column, str]):
+		super().__init__(DatePartType.Month, expr)
+		return
+
+
+class Day(DatePart):
+	def __init__(self, expr: Union[Expr, Column, str]):
+		super().__init__(DatePartType.Day, expr)
+		return
+
+
+class Hour(DatePart):
+	def __init__(self, expr: Union[Expr, Column, str]):
+		super().__init__(DatePartType.Hour, expr)
+		return
+
+
+class Minute(DatePart):
+	def __init__(self, expr: Union[Expr, Column, str]):
+		super().__init__(DatePartType.Minute, expr)
+		return
+
+
+class Second(DatePart):
+	def __init__(self, expr: Union[Expr, Column, str]):
+		super().__init__(DatePartType.Second, expr)
+		return
+
+
+class MilliSecond(DatePart):
+	def __init__(self, expr: Union[Expr, Column, str]):
+		super().__init__(DatePartType.MilliSecond, expr)
+		return
+
+
+class MicroSecond(DatePart):
+	def __init__(self, expr: Union[Expr, Column, str]):
+		super().__init__(DatePartType.MicroSecond, expr)
+		return
+
+
+class Quarter(DatePart):
+	def __init__(self, expr: Union[Expr, Column, str]):
+		super().__init__(DatePartType.Quarter, expr)
+		return
+
+
+class Week(DatePart):
+	def __init__(self, expr: Union[Expr, Column, str]):
+		super().__init__(DatePartType.Week, expr)
+		return
+
+
+class DayOfWeek(DatePart):
+	def __init__(self, expr: Union[Expr, Column, str]):
+		super().__init__(DatePartType.DayOfWeek, expr)
+		return
+
+
+class DayOfWeekISO(DatePart):
+	def __init__(self, expr: Union[Expr, Column, str]):
+		super().__init__(DatePartType.DayOfWeekISO, expr)
+		return
+
+
+class DayOfYear(DatePart):
+	def __init__(self, expr: Union[Expr, Column, str]):
+		super().__init__(DatePartType.DayOfYear, expr)
+		return
+
+
+class Timezone(DatePart):
+	def __init__(self, expr: Union[Expr, Column, str]):
+		super().__init__(DatePartType.Timezone, expr)
+		return
+
+
+class TimezoneHour(DatePart):
+	def __init__(self, expr: Union[Expr, Column, str]):
+		super().__init__(DatePartType.TimezoneHour, expr)
+		return
+
+
+class TimezoneMinute(DatePart):
+	def __init__(self, expr: Union[Expr, Column, str]):
+		super().__init__(DatePartType.TimezoneMinute, expr)
+		return
+
+
+class TimezoneHourMinute(DatePart):
+	def __init__(self, expr: Union[Expr, Column, str]):
+		super().__init__(DatePartType.TimezoneHourMinute, expr)
+		return
+
+
+class Millenium(DatePart):
+	def __init__(self, expr: Union[Expr, Column, str]):
+		super().__init__(DatePartType.Millenium, expr)
+		return
+
+
+class Century(DatePart):
+	def __init__(self, expr: Union[Expr, Column, str]):
+		super().__init__(DatePartType.Centry, expr)
+		return
+
+
+class Decade(DatePart):
+	def __init__(self, expr: Union[Expr, Column, str]):
+		super().__init__(DatePartType.Decade, expr)
+		return
+
+
+class EPoch(DatePart):
+	def __init__(self, expr: Union[Expr, Column, str]):
+		super().__init__(DatePartType.EPoch, expr)
+		return
